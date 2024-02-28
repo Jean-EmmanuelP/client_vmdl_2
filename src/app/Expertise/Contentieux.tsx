0f8c9021f9280520e35911765b38fd7ec1993728
Jean-EmmanuelP <jperramal@gmail.com>
2024-02-28 22:40:52 +0100

    added only an img its simpler

--- src/app/Expertise/Contentieux.tsx
+++ src/app/Expertise/Contentieux.tsx
@@ -1,36 +1,62 @@
-import React, { useEffect, useRef, useState } from "react";
+import { LangueCode, useExpertise, useSection } from "../utils/Contextboard";
 import { motion } from "framer-motion";
-import { useExpertise, useSection } from "../utils/Contextboard";
+import { useEffect, useRef, useState } from "react";
 import { useData } from "../utils/DataContext";
-
-// Assurez-vous que LangueCode est correctement importé ou défini quelque part dans votre projet
-type LangueCode = "FR" | "EN" | "IT" | "ES" | "عربي" | "PT" | "DE" | "中文";
 
 export default function Contentieux() {
   const { subExpertise } = useExpertise();
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [textOpacity, setTextOpacity] = useState(0);
-  const [videoError, setVideoError] = useState(false);
   const { langueCourante, mediaPaths } = useSection();
   const { data } = useData();
-  const isMobile = window.innerWidth <= 768;
-
-  const isIOS = (): boolean => /iPad|iPhone|iPod/.test(navigator.userAgent);
-
   useEffect(() => {
-    const handleVideoError = () => setVideoError(true);
-    const videoCurrent = videoRef.current;
-    if (videoCurrent) {
-      videoCurrent.addEventListener("error", handleVideoError);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          videoRef.current?.play();
+          setTextOpacity(1);
+        } else {
+          videoRef.current?.pause();
+          setTextOpacity(0);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
     }
+
     return () => {
-      if (videoCurrent) {
-        videoCurrent.removeEventListener("error", handleVideoError);
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
       }
     };
   }, []);
+  useEffect(() => {
+    const videoElement = videoRef.current;
 
-  if (!data) return null;
+    const handleTimeUpdate = () => {
+      if (videoElement && videoElement.currentTime >= 12) {
+        setTextOpacity(1);
+        videoElement.removeEventListener("timeupdate", handleTimeUpdate);
+      }
+    };
+
+    if (videoElement) {
+      videoElement.addEventListener("timeupdate", handleTimeUpdate);
+    }
+
+    return () => {
+      if (videoElement) {
+        videoElement.removeEventListener("timeupdate", handleTimeUpdate);
+      }
+    };
+  }, [videoRef]);
+  if (!data) {
+    return null;
+  }
 
   const langCodeMap: { [key in LangueCode]: string } = {
     FR: "fr",
@@ -42,12 +68,13 @@ export default function Contentieux() {
     DE: "de",
     中文: "中文",
   };
-
   const langCode =
     langCodeMap[langueCourante as LangueCode] || langCodeMap["FR"];
-  const { content } = data[langCode]?.section_3?.box_2 || { content: "" };
+  const { content } = data[langCode].section_3.box_2;
+  const isMobile = window.innerWidth <= 768;
 
   const formatContent = (content: string): string => {
+    // Diviser le contenu à chaque point, puis rejoindre les éléments avec un '<br>' pour un saut de ligne HTML
     return (
       content
         .split(".")
@@ -55,9 +82,11 @@ export default function Contentieux() {
         .join(".<br><br>") + "."
     );
   };
-
   const formattedContent = formatContent(content);
-
+  function convertToMp4Path(webmPath: string) {
+    return webmPath.replace(".webm", ".mp4");
+  }
+  const isIOS = (): boolean => /iPad|iPhone|iPod/.test(navigator.userAgent);
   return (
     <motion.div
       animate={{ x: subExpertise === "contentieux" ? "0vw" : "100vw" }}
@@ -67,31 +96,34 @@ export default function Contentieux() {
         isMobile ? "h-[110vh]" : "h-full"
       } flex justify-center items-center text-blanc`}
     >
-      {isIOS() && videoError ? (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: textOpacity }}
+        transition={{ delay: 12, duration: 0.3 }}
+        className="z-10"
+      >
         <div
-          className="w-full h-full bg-center bg-cover flex justify-center items-center"
-          style={{
-            backgroundImage: `url(${mediaPaths.vosges.replace(
-              ".webm",
-              ".jpeg"
-            )})`,
-          }} // Remplacer .webm par .jpeg pour le poster
-        >
-          <div
-            className="p-2 sm:p-10 flex flex-col gap-2 justify-center items-center text-center sm:text-xl text-white tracking-wide rounded-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100/20 shadow-2xl font-light text-sm"
-            dangerouslySetInnerHTML={{ __html: formattedContent }}
-          ></div>
-        </div>
-      ) : (
+          className="p-2 sm:p-10 absolute top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 flex flex-col gap-2 justify-center items-center text-center sm:text-xl text-white tracking-wide rounded-md bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100/20 shadow-2xl font-light text-sm"
+          dangerouslySetInnerHTML={{ __html: formattedContent }}
+        ></div>
+      </motion.div>
+      {isIOS() && (
+        <img
+          src="/images/vosges.jpeg"
+          alt="Vosges"
+          className="w-full h-full object-cover bg-black"
+        />
+      )}
+      {!isIOS() && (
         <video
           ref={videoRef}
           playsInline
-          className="w-full h-full object-cover bg-black"
-          poster={mediaPaths.vosges.replace(".webm", ".jpeg")} // Utiliser le poster pour tous les cas
+          className="w-full h-full object-cover bg-red-500"
+          // poster={`/images/vosges.jpeg`}
         >
-          <source src={mediaPaths.vosges} type="video/webm" />
+          <source src={`${mediaPaths.vosges}`} type="video/webm" />
           <source
-            src={mediaPaths.vosges.replace(".webm", ".mp4")}
+            src={`${convertToMp4Path(mediaPaths.vosges)}`}
             type="video/mp4"
           />
         </video>
