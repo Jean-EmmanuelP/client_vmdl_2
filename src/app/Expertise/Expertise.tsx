774efe06f9ad879e07d1f865ef6ab725a7822cc1
Your Name <you@example.com>
2024-03-14 22:46:27 +0100

    doing the rideau but problem

--- src/app/Expertise/Expertise.tsx
+++ src/app/Expertise/Expertise.tsx
@@ -16,21 +16,20 @@ export default function Expertise() {
   const { langueCourante, isMobile, setIsHoveringExpertiseButton } =
     useSection();
   const { data } = useData();
-  const [isActiveContent, setIsActiveContent] = useState<
-    null | "conseil" | "contentieux" | "affaires"
-  >(null);
   const [isVisible, setIsVisible] = useState(true);
+  const [showRideau, setShowRideau] = useState(false);
+
   function handleClick(
-    activeContent: null | "conseil" | "contentieux" | "affaires"
+    activeContent: "conseil" | "contentieux" | "affaires" | null
   ) {
     setIsHoveringExpertiseButton("none");
-
-    setIsVisible(false);
+    setShowRideau(true);
 
     setTimeout(() => {
-      setIsActiveContent(activeContent);
+      setShowRideau(false);
+      setIsVisible(false);
       setSubExpertise(activeContent);
-    }, 500);
+    }, 2500);
   }
   const memoizedTextCycle = useMemo(() => {
     return (
@@ -94,135 +93,125 @@ export default function Expertise() {
   const { title: box_1_title } = box_1;
   const { title: box_2_title } = box_2;
   const { title: box_3_title } = box_3;
-  const variants = {
-    visible: {
-      opacity: 1,
-      transition: { duration: 0.5 },
-    },
-    hidden: {
-      opacity: 0,
-      transition: { duration: 0.5 },
-    },
-  };
-
-
-
   if (!isMobile) {
     return (
       <div className={`relative w-full h-[300vh] sm:h-full bg-blanc`}>
-        <motion.div
-          initial="hidden"
-          animate={isVisible ? "visible" : "hidden"}
-          variants={variants}
-          className={`relative w-full h-full flex justify-center items-center`}
+        <div
+          className={`rideau ${showRideau ? "rideau-animation" : ""}`}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            transform: "translateY(-100%)",
+            backgroundColor: "black",
+            zIndex: 3000,
+          }}
         >
+        </div>
+        <div
+          className={`relative w-full px-[10%] h-full gap-[4vw] flex justify-center items-center`}
+        >
+          {/* title */}
           <div
-            className={`relative w-full px-[10%] h-full gap-[4vw] flex justify-center items-center fade-transition ${isVisible ? "fade-visible" : "fade-hidden z-0"
-              }`}
+            className={`absolute top-[6%] sm:top-[12%] text-[30px] sm:text-[40px] left-1/2 -translate-x-1/2 -translate-y-1/2 sm:title font-light`}
           >
-            {/* title */}
-            <div
-              className={`absolute top-[6%] sm:top-[12%] text-[30px] sm:text-[40px] left-1/2 -translate-x-1/2 -translate-y-1/2 sm:title font-light`}
-            >
-              {title}
-            </div>
-            {/* first Box */}
-            <div
-              className={`expertiseWrapper wrapper1 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden`}
-              onMouseEnter={() => {
-                setIsHoveringExpertiseButton("conseil");
-              }}
-              onMouseLeave={() => {
-                setIsHoveringExpertiseButton("none");
-              }}
-              onClick={() => {
-                handleClick("conseil");
-              }}
-            >
-              <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
-                <h1 className="top-[10%] left-[20%] absolute uppercase">{box_1_title}</h1>
-                <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
-                  <span className="pl-[20%] text-line">VMDL vous conseille</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                </div>
+            {title}
+          </div>
+          {/* first Box */}
+          <div
+            className={`expertiseWrapper wrapper1 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden`}
+            onMouseEnter={() => {
+              setIsHoveringExpertiseButton("conseil");
+            }}
+            onMouseLeave={() => {
+              setIsHoveringExpertiseButton("none");
+            }}
+            onClick={() => {
+              handleClick("conseil");
+            }}
+          >
+            <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
+              <h1 className="top-[10%] left-[20%] absolute uppercase">
+                {box_1_title}
+              </h1>
+              <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
+                <span className="pl-[20%] text-line">VMDL vous conseille</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
               </div>
             </div>
-            {/* second Box */}
-            <div
-              className="expertiseWrapper wrapper2 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden"
-              onMouseEnter={() => {
-                setIsHoveringExpertiseButton("contentieux");
-              }}
-              onMouseLeave={() => {
-                setIsHoveringExpertiseButton("none");
-              }}
-              onClick={() => {
-                handleClick("contentieux");
-              }}
-            >
-              <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
-                <h1 className="top-[10%] left-[20%] absolute uppercase">{box_2_title}</h1>
-                <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
-                  <span className="pl-[20%] text-line">VMDL vous conseille</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                </div>
+          </div>
+          {/* second Box */}
+          <div
+            className="expertiseWrapper wrapper2 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden"
+            onMouseEnter={() => {
+              setIsHoveringExpertiseButton("contentieux");
+            }}
+            onMouseLeave={() => {
+              setIsHoveringExpertiseButton("none");
+            }}
+            onClick={() => {
+              handleClick("contentieux");
+            }}
+          >
+            <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
+              <h1 className="top-[10%] left-[20%] absolute uppercase">
+                {box_2_title}
+              </h1>
+              <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
+                <span className="pl-[20%] text-line">VMDL vous conseille</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
               </div>
             </div>
-            {/* third Box */}
-            <div
-              className="expertiseWrapper wrapper3 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden"
-              onMouseEnter={() => {
-                setIsHoveringExpertiseButton("affaires");
-              }}
-              onMouseLeave={() => {
-                setIsHoveringExpertiseButton("none");
-              }}
-              onClick={() => {
-                handleClick("affaires");
-              }}
-            >
-              <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
-                <h1 className="top-[10%] left-[20%] absolute uppercase">{box_3_title}</h1>
-                <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
-                  <span className="pl-[20%] text-line">VMDL vous conseille</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                  <span className="pl-[20%] text-line">au jour le jour</span>
-                </div>
+          </div>
+          {/* third Box */}
+          <div
+            className="expertiseWrapper wrapper3 relative w-full sm:w-1/3 h-[80%] sm:h-[55%] overflow-hidden"
+            onMouseEnter={() => {
+              setIsHoveringExpertiseButton("affaires");
+            }}
+            onMouseLeave={() => {
+              setIsHoveringExpertiseButton("none");
+            }}
+            onClick={() => {
+              handleClick("affaires");
+            }}
+          >
+            <div className="absolute inset-0 w-full h-full text-blanc z-[2001]">
+              <h1 className="top-[10%] left-[20%] absolute uppercase">
+                {box_3_title}
+              </h1>
+              <div className="text-wrapper absolute bottom-[20%] w-full -translate-y-1/2">
+                <span className="pl-[20%] text-line">VMDL vous conseille</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
+                <span className="pl-[20%] text-line">au jour le jour</span>
               </div>
             </div>
-            {/* <div className="absolute w-full px-[14%] bottom-[13%] mb-2">
+          </div>
+          {/* <div className="absolute w-full px-[14%] bottom-[13%] mb-2">
               {memoizedTextCycle}
               <div className="border-b border-noir"></div>
             </div> */}
-          </div>
-        </motion.div>
+        </div>
         <div id="cursor-root"></div>
         {/* Contenu actif */}
-        <motion.div
-          initial="hidden"
-          animate={isActiveContent ? "visible" : "hidden"}
-          variants={variants}
-          className="absolute inset-0 w-full h-full z-10"
-        >
-          {activeContent === 'conseil' && <Conseil />}
-          {activeContent === 'contentieux' && <Contentieux />}
-          {activeContent === 'affaires' && <Affaires />}
-        </motion.div>
+        {activeContent === "conseil" && <Conseil />}
+        {activeContent === "contentieux" && <Contentieux />}
+        {activeContent === "affaires" && <Affaires />}
       </div>
     );
   } else {
     return (
-      <div className={`border-2 w-full h-[150vh] bg-blanc relative`} ref={expertiseRef}>
-        {/* <motion.div
-          initial="hidden"
-          animate={isIntersecting ? "visible" : "hidden"}
-          variants={variants}
-        > */}
+      <div
+        className={`border-2 w-full h-[150vh] bg-blanc relative`}
+        ref={expertiseRef}
+      >
         {/* title */}
         <div
           className={`absolute top-[10%] text-[20px] left-1/2 -translate-x-1/2 -translate-y-1/2 font-light`}
@@ -232,18 +221,23 @@ export default function Expertise() {
         <div className="absolute top-[50%] w-[80%] h-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-6 justify-center items-center flex-col">
           {/* first box */}
           <div className="w-full h-1/3 bg-blue-500 expertiseWrapper wrapper1 relative">
-            <h1 className="top-[10%] left-[20%] absolute uppercase">{box_1_title}</h1>
+            <h1 className="top-[10%] left-[20%] absolute uppercase">
+              {box_1_title}
+            </h1>
           </div>
           {/* second box */}
           <div className="w-full h-1/3 bg-blue-500 expertiseWrapper wrapper2 relative">
-            <h1 className="top-[10%] left-[20%] absolute uppercase">{box_2_title}</h1>
+            <h1 className="top-[10%] left-[20%] absolute uppercase">
+              {box_2_title}
+            </h1>
           </div>
           {/* third box */}
           <div className="w-full h-1/3 bg-blue-500 expertiseWrapper wrapper3 relative">
-            <h1 className="top-[10%] left-[20%] absolute uppercase">{box_3_title}</h1>
+            <h1 className="top-[10%] left-[20%] absolute uppercase">
+              {box_3_title}
+            </h1>
           </div>
         </div>
-        {/* </motion.div> */}
       </div>
     );
   }
