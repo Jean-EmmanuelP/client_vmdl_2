191ee1d5eaa4384f25ff22e381a80acc3f781ba2
Your Name <you@example.com>
2024-03-19 08:47:44 +0100

    finished the carreer page

--- src/app/Carriere/Carriere.tsx
+++ src/app/Carriere/Carriere.tsx
@@ -3,23 +3,33 @@ export default function Carriere() {
     <div className="w-full h-full bg-blanc flex items-center justify-center">
       <div className="w-[40%] h-full bg-red-500">
         <img
-          src="/images/paris_1.JPG"
+          src="/images/court-hammer-books-judgment-law-concept.jpg"
           alt=""
           className="object-cover w-full h-full"
         />
       </div>
       <div className="w-[60%] h-full flex items-center justify-center">
         <p className="w-[70%] text-left">
-          Bonnant & Associes recherche en permanence des avocats et
+          VMDL recherche en permanence des avocats et
           avocats-stagiaires de talent.
-          <br/>
           <br />
-          Si vous souhaitez rejoindre notee cabinet et disposez d&apos;excellentes
-          references tout en etant au benefice dun parcours academique
-          exemplaire, n&apos;hesitez pas a nous faire parvenir votre dossier.
-          <br/>
-          <br />    
+          <br />
+          Si vous souhaitez rejoindre notee cabinet et disposez
+          d&apos;excellentes references tout en etant au benefice dun parcours
+          academique exemplaire, n&apos;hesitez pas a nous faire parvenir votre
+          dossier.
+          <br />
+          <br />
           Toutes les candidatures seront traitees de maniere confidentielle.
+          Pour candidatez, envoyez un mail a l'addresse ci-dessous:
+          <br />
+          <br />
+          <a
+            className="underline transition duration-70"
+            href="mailto:cabinet@vmdl.ai"
+          >
+            cabinet@vmdl.ai
+          </a>
         </p>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -333,7 +333,7 @@ export default function App() {
               ) : (
                 <>
                   <Carriere />
-                  <Contact />
+                  {/* <Contact /> */}
                   <Footer />
                 </>
               )}
