d4ba69cdbe744d2b81c6da9fa88308b17fd6bf74
Jean-Emmanuel Perramant <jperrama@gmail.com>
2024-04-04 23:47:02 +0200

    trying another way

--- src/app/Components/BackgroundEiffel.tsx
+++ src/app/Components/BackgroundEiffel.tsx
@@ -7,73 +7,48 @@ const BackgroundEiffel: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const { isMobile, mediaPaths } = useSection();
   const videoSource = isMobile ? `/videos/mobile/paris/paris_high_mobile.mp4` : "/videos/laptop/paris/paris_high.webm";
+  const [isPaused, setIsPaused] = useState<boolean>(false);
   function convertToMp4Path(webmPath: string) {
     const webmToMp4 = webmPath.replace(".webm", ".mp4");
     return webmToMp4;
   }
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
-
-  // Vous pouvez essayer de détecter quand la vidéo est prête à jouer
-  const handleVideoPlay = () => {
-    setIsVideoPlaying(true);
-  };
-
-  // Et quand elle est mise en pause
-  const handleVideoPause = () => {
-    setIsVideoPlaying(false);
-  };
 
   useEffect(() => {
-    const videoElement = videoRef.current;
-    if (videoElement) {
-      videoElement.addEventListener('play', handleVideoPlay);
-      videoElement.addEventListener('pause', handleVideoPause);
-
-      // Auto-play ne fonctionne pas sur tous les appareils iOS en mode d'économie d'énergie,
-      // donc nous tentons de jouer la vidéo après un petit délai
-      setTimeout(() => {
-        videoElement.play().catch(() => {
-          // La vidéo n'a pas joué, peut-être à cause du mode d'économie d'énergie
-          console.log("La vidéo ne peut pas jouer automatiquement. Elle pourrait être bloquée par le mode économie d'énergie.");
-        });
-      }, 1000);
-    }
-
-    // Nettoyage de l'effet
-    return () => {
-      if (videoElement) {
-        videoElement.removeEventListener('play', handleVideoPlay);
-        videoElement.removeEventListener('pause', handleVideoPause);
+    setTimeout(() => {
+      if (videoRef.current && videoRef.current.paused) {
+        setIsPaused(true);
       }
-    };
-  }, []);
+    }, 2000);
+  }, [])
+
+
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full z-[-1] bg-cover ${isMobile ? `bg-[url('/images/background.png')]` : `bg-[url('/images/background.png')]`}`}
       id="videoContainer"
     >
       {
-        !isVideoPlaying &&
-        <video
-          ref={videoRef}
-          src={videoSource}
-          playsInline
-          autoPlay
-          loop
-          muted
-          className="absolute w-full h-full object-cover object-center"
-          onPlay={handleVideoPlay}
-          onPause={handleVideoPause}
-        >
-          <source
-            src={`${convertToMp4Path(mediaPaths.paris)}`}
-            type="video/mp4"
-          />
-          <source src={`${mediaPaths.paris}`} type="video/webm" />
-        </video>
+        (!isMobile && !isPaused) &&
+          (
+            <video
+              ref={videoRef}
+              src={videoSource}
+              playsInline
+              autoPlay
+              loop
+              muted
+              className="absolute w-full h-full object-cover object-center"
+            >
+              <source
+                src={`${convertToMp4Path(mediaPaths.paris)}`}
+                type="video/mp4"
+              />
+              <source src={`${mediaPaths.paris}`} type="video/webm" />
+            </video>
+          )
+
       }
     </div>
   );
-};
-
+}
 export default BackgroundEiffel;
